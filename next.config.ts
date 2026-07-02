4f29ee38706f6a10dc12c0ae4f7bbba6d456e8d4
ioannisv-ops <ioannisv-ops@protonmail.com>
2026-07-03 01:10:41 +0300

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
   trailingSlash: true,
+
+  basePath: isProd ? "/main" : "",
+  assetPrefix: isProd ? "/main/" : "",
 };
 
 export default nextConfig;
