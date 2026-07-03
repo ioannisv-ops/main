7d8e8730816ce9e6853dc5c24512e1f0cf122ec3
ioannisv-ops <ioannisv-ops@protonmail.com>
2026-07-03 23:50:20 +0300

    Fix Vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
   images: {
     unoptimized: true,
   },
-
-  trailingSlash: true,
-
-  // Required for GitHub Pages project site
-  basePath: "/main",
-  assetPrefix: "/main/",
 };
 
 export default nextConfig;
